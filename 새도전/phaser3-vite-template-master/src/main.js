c206a1ec59719de606aa54b20d31e5e3b3353254
DESKTOP-M0J6PE5\amd2600k <qwq12q12@gmail.com>
2024-10-10 16:41:30 +0900

    [최지은]2024-10-10 - 튜툐리얼 대로 이미지 표현 성공

--- 새도전/phaser3-vite-template-master/src/main.js
+++ 새도전/phaser3-vite-template-master/src/main.js
@@ -1,19 +1,21 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './HelloWorldScene'
+import {GameScene} from './GameScene'
 
 const config = {
 	type: Phaser.AUTO,
 	parent: 'app',
 	width: 800,
 	height: 600,
-	physics: {
+	physics: { /*게임의 물리효과를 작용하는 프로퍼티*/
 		default: 'arcade',
+		/*phaser는 phaser에서 만든 자체 물리엔진인 arcade와 
+		상용화 되어있는 matter라는 2개의 물리엔진을 지원한다.*/
 		arcade: {
 			gravity: { y: 200 },
 		},
 	},
-	scene: [HelloWorldScene],
+	scene: [GameScene],
 }
 
 export default new Phaser.Game(config)
